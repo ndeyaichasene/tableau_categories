c754ad8130d188dc75c3e661eb139a0eca78a0e1
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-06-30 22:46:58 +0000

    feat: Q1 - initialisation de la liste de 2 categories

--- imperative.php
+++ imperative.php
@@ -1,2 +1,28 @@
 <?php
 
+$categories =[
+            0=> [
+                'nom'=>'alimentaire',
+                'code'=> 'C01',
+                'produits'=>[
+                        0=>[
+                            'nom'=>'lait',
+                            'reference'=>'ref1',
+                            'prix'=> 150,
+                            'quantite'=>200
+                        ],
+                        1=>[
+                            'nom'=>'sucre',
+                            'reference'=>'ref2',
+                            'prix'=> 50,
+                            'quantite'=>1000
+                        ],
+                ],
+            ],
+            1=> [
+                'nom'=>'hygiene',
+                'code'=> 'C02',
+                'produits'=>[]   
+            ]
+
+];
