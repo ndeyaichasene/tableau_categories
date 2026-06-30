e70ffc7d213506da606eb5493331451e158cbce7
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-06-30 23:58:04 +0000

    feat: Q3 - Ajout d'une catégorie avec validation du code et du nom

--- imperative.php
+++ imperative.php
@@ -27,10 +27,46 @@ $categories =[
 
 ];
 
-//2-
+//2- 
+
 foreach ($categories as $index => $categorie) {
     if(empty($categorie['produits'])){
         echo "la categorie ".$categorie['nom']." n'a pas de produit\n";
     }
     
 }
+
+//3 
+
+do {
+    $codeExiste = false;
+    $code = readline("entrer le code ");
+    if(empty($code)){
+        echo "champs obligatoire \n";
+    }else{
+        foreach ($categories as $key => $categorie) {
+           if($categorie['code']==$code){
+                $codeExiste = true;
+               echo "le code exist ";
+           }
+        }
+    }
+
+} while (empty($code) || $codeExiste);
+
+do {
+    $nom = readline("Entrer le nom : ");
+
+    if (empty($nom)) {
+        echo "Champ obligatoire.\n";
+    }
+
+} while (empty($nom));
+
+$categories[] = [
+    'nom' => $nom,
+    'code' => $code,
+    'produits' => []
+];
+
+echo "Catégorie enregistrée avec succès.\n";
