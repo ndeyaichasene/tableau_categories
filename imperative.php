56b1cfdd2234055505a00807b103bfafca0ae10b
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-07-01 01:12:44 +0000

    feat: Q1 - fonction initialisation liste de 2 categories

--- imperative.php
+++ imperative.php
@@ -1,2 +1,34 @@
-<?php
 
+<?php
+//1-
+
+function initialiserCategories() {
+    return [
+        [
+            'nom' => 'alimentaire',
+            'code' => 'C01',
+            'produits' => [
+                [
+                    'nom' => 'lait',
+                    'reference' => 'ref1',
+                    'prix' => 150,
+                    'quantite' => 200
+                ],
+                [
+                    'nom' => 'sucre',
+                    'reference' => 'ref2',
+                    'prix' => 50,
+                    'quantite' => 1000
+                ]
+            ]
+        ],
+        [
+            'nom' => 'hygiene',
+            'code' => 'C02',
+            'produits' => []
+        ]
+    ];
+}
+
+$categories = initialiserCategories();
+print_r($categories);
